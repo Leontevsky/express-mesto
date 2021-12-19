dce1fd1cbd866d15d71b0628030d82cc4d963f88
leontevsky <alexander.leontevsky@gmail.com>
2021-12-19 18:42:04 +0300

    Подключился к Mongo

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// Сервер mongo запускают командой mongod или brew services start mongodb-community@4.4 до запуска Node.js:
+
 // подключение express
-const express = require('express');
+const express = require("express");
+// подключение mongoose
+const mongoose = require("mongoose");
 //  Настройка порта, который слушает приложение. По умолчанию 3000. Взяли из переменной окружения
 const { PORT = 3000, BASE_PATH } = process.env; // или 2 вариант: const PORT = process.env.PORT || 3000
 // создание приложения методом express
 const app = express();
 
+// подключаемся к серверу mongo
+mongoose.connect("mongodb://localhost:27017/mestodb", {});
+
 // Прослушиваем подключение на порту 3000
 app.listen(PORT, () => {
-  console.log('Ссылка на сервер:');
-  console.log(process.env);
+  console.log("Ссылка на сервер:");
+  console.log(BASE_PATH);
   console.log(`Приложение App слушает порт: ${PORT}`);
 });
